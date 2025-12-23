690ed9ba449d40368f5f10b048ef8e02e10a76a9
Dayalan Jayaseelan <dayalanmidhun@gmail.com>
2025-12-23 04:53:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5969953193-9ce1e",
+  "appId": "1:923902434036:web:422b755d25844cffd21ca1",
+  "apiKey": "AIzaSyDFRmWlofERd3u4Mtan3mGUhGeHBqVYJDI",
+  "authDomain": "studio-5969953193-9ce1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "923902434036"
+};
